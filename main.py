fbd7a049f7f630327e6aff9f476e1675e7976a88
gaspard.vandecasteele <gaspard.vandecasteele@gmail.com>
2022-10-07 15:12:15 +0200

    fix

--- main.py
+++ main.py
@@ -7,47 +7,51 @@ import os
 
 
 class Nmapscan:
-    def __init__(self, nmap_exec: str, masscan_output_map: object, scan_parameters: [str]):
+    def __init__(self, nmap_exec: str, masscan_output_path: str, scan_parameters: [str]):
         self.nmap_exec = nmap_exec
         self.scan_parameters = scan_parameters
-        self.temporary_scan_file = "./tmp_nmap.txt"
+        self.masscan_output_path = masscan_output_path
 
     def start_scan(self):
-        for ip in self.masscan_output_map["ip"]:
-            with open(self.temporary_scan_file, 'a') as temporary_file:
-                temporary_file.write(ip + "\n")
         subprocess.call([self.nmap_exec, *self.scan_parameters,
-                        "-iL", self.temporary_scan_file])
+                        "-iL", self.masscan_output_path])
 
-    def destruct():
-        os.remove(self.temporary_scan_file)
+    def destruct(self):
+        os.remove(self.masscan_output_path)
 
 
 class Masscan:
-    def __init__(self, masscan_exec: str, scan_parameters: [str], output_bin_path: str, output_json_path: str):
+    def __init__(self, masscan_exec: str, scan_parameters: [str], output_bin_path: str, output_json_path: str, output_plain_path: str):
         self.masscan_exec = masscan_exec
         self.scan_parameters = scan_parameters
         self.output_json_path = output_json_path
         self.output_bin_path = output_bin_path
+        self.output_plain_path = output_plain_path
 
     def start_scan(self):
         return subprocess.call([self.masscan_exec, *self.scan_parameters, "-oB", self.output_bin_path])
 
-    def get_output_file(self):
+    def transform_output_file(self):
         subprocess.call(
             [self.masscan_exec, "--readscan",
              self.output_bin_path, "-oJ", self.output_json_path])
-        with open(self.output_json_path, 'r') as json_file:
-            return json.load(json_file)
+        with open(self.output_json_path, 'r') as json_file, open(self.output_plain_path, 'a') as plain_file:
+            for line in json_file:
+                try:
+                    data = json.loads(line)
+                except Exception as err:
+                    continue
+                plain_file.write(f"{data['ip']}\n")
 
-    def destruct():
-        os.remove(output_json_path)
+    def destruct(self):
+        os.remove(self.output_json_path)
 
 
 def main():
     masscan_executable = "masscan"
     scan_file_binary = "./masscan-open-proxy.bin"
     scan_file_json = "./masscan-open-proxy.json"
+    scan_file_plain = "./masscan-open-proxy.txt"
     blacklist_file = "./ipblacklist.txt"
     masscan_scan_arguments = ["-p", "3128", "--excludefile",
                               blacklist_file, "--open-only",
@@ -55,9 +59,9 @@ def main():
                               "--rate", "500000", "0.0.0.0/0"]
 
     masscan = Masscan(masscan_executable,
-                      masscan_scan_arguments, scan_file_binary, scan_file_json)
+                      masscan_scan_arguments, scan_file_binary, scan_file_json, scan_file_plain)
     # masscan.start_scan()
-    masscan_output_object = masscan.get_output_file()
+    masscan.transform_output_file()
     masscan.destruct()
 
     nmap_normal_output_file = "./open-proxy.txt"
@@ -66,7 +70,7 @@ def main():
     nmap_scan_arguments = ["-vvv", "--script",
                            "http-open-proxy.nse", "-p", "3128", "--open"
                            "-Pn", "-sS", "-oN", nmap_normal_output_file, "-oX", nmap_xml_output_file]
-    nmap = Nmapscan(nmap_executable, masscan_output_object,
+    nmap = Nmapscan(nmap_executable, scan_file_plain,
                     nmap_scan_arguments)
     nmap.start_scan()
     nmap.destruct()

7bf6196868bfd654bc05a2d372ea3772fcc40cd7
gaspard.vandecasteele <gaspard.vandecasteele@gmail.com>
2022-10-07 15:39:23 +0200

    fix

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ def main():
     nmap_normal_output_file = "./open-proxy.txt"
     nmap_xml_output_file = "./open-proxy.xml"
     nmap_executable = "nmap"
-    nmap_scan_arguments = ["-vvv", "-n", "--script", "-T4"
+    nmap_scan_arguments = ["-vvv", "-n", "-T4", "--script"
                            "http-open-proxy.nse", "-p", "3128", "--open",
                            "-Pn", "-sS", "-oN", nmap_normal_output_file, "-oX", nmap_xml_output_file]
     nmap = Nmapscan(nmap_executable, scan_file_plain,

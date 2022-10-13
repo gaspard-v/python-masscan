006f246580dcd76d341991f7153ac9ecb11631a6
gaspard.vandecasteele <gaspard.vandecasteele@gmail.com>
2022-10-13 15:15:34 +0200

    add async

--- main.py
+++ main.py
@@ -8,6 +8,40 @@ from datetime import datetime
 import tarfile
 import asyncio
 import sys
+import urllib3
+
+
+class Proxyscan:
+    def __init__(self, proxy_list_file: str):
+        self.proxy_list_file = proxy_list_file
+
+    async def test_proxy(self, proxy: str, port: str):
+        chrome_header = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
+        url_test = "https://icanhazip.com"
+        expected_result = proxy
+        headers = urllib3.make_headers(
+            user_agent=chrome_header)
+        timeout = urllib3.util.Timeout(connect=10.0, read=10.0)
+        retry = urllib3.util.Retry(connect=2, read=2, redirect=5)
+        try:
+            proxy_manager = urllib3.ProxyManager(
+                f"http://{proxy}:{port}", proxy_headers=headers, timeout=timeout, retries=retry, cert_reqs='CERT_NONE')
+            r = proxy_manager.request("GET", url_test)
+            if r == expected_result:
+                return True
+        except Exception as err:
+            print(err, file=sys.stderr)
+            pass
+        return False
+
+    async def test_proxies(self):
+        tasks = []
+        with open(self.proxy_list_file, 'r') as proxy_file:
+            for line in proxy_file:
+                [proxy, port] = line.split(":")
+                tasks.append(asyncio.create_task(self.test_proxy(proxy, port)))
+
+        await asyncio.gather(tasks)
 
 
 class Nmapscan:
@@ -45,7 +79,7 @@ class Masscan:
                     data = json.loads(line)
                 except Exception as err:
                     continue
-                plain_file.write(f"{data['ip']}\n")
+                plain_file.write(f"{data['ip']}:3128\n")
 
     async def destruct(self):
         os.remove(self.output_json_path)
@@ -66,35 +100,40 @@ async def logrotate(files: [str]):
 
 
 async def main():
-    masscan_executable = "masscan"
-    scan_file_binary = "./masscan-open-proxy.bin"
-    scan_file_json = "./masscan-open-proxy.json"
-    scan_file_plain = "./masscan-open-proxy.txt"
-    blacklist_file = "./ipblacklist.txt"
-    masscan_scan_arguments = ["-p", "3128", "--excludefile",
-                              blacklist_file, "--open-only",
-                              "--exclude", "255.255.255.255", "--capture", "html",
-                              "--rate", "500000", "0.0.0.0/0"]
-    nmap_executable = "nmap"
+    # masscan_executable = "masscan"
+    # scan_file_binary = "./masscan-open-proxy.bin"
+    # scan_file_json = "./masscan-open-proxy.json"
+    # scan_file_plain = "./masscan-open-proxy.txt"
+    # blacklist_file = "./ipblacklist.txt"
+    # masscan_scan_arguments = ["-p", "3128", "--excludefile",
+    #                           blacklist_file, "--open-only",
+    #                           "--exclude", "255.255.255.255", "--capture", "html",
+    #                           "--rate", "500000", "0.0.0.0/0"]
+    # nmap_executable = "nmap"
 
-    while(True):
-        masscan = Masscan(masscan_executable,
-                          masscan_scan_arguments, scan_file_binary, scan_file_json, scan_file_plain)
+    # while True:
+    #     masscan = Masscan(masscan_executable,
+    #                       masscan_scan_arguments, scan_file_binary, scan_file_json, scan_file_plain)
 
-        await masscan.start_scan()
-        await masscan.transform_output_file()
+    #     await masscan.start_scan()
+    #     await masscan.transform_output_file()
+    #     masscan_destruct = asyncio.create_task(masscan.destruct())
 
-        date_time = datetime.today().strftime("%d-%m-%Y_%H-%M-%S")
-        nmap_normal_output_file = f"./open-proxy_{date_time}.txt"
-        nmap_xml_output_file = f"./open-proxy_{date_time}.xml"
-        nmap_scan_arguments = ["-vvv", "-n", "-T4", "--script", "http-open-proxy.nse", "-p", "3128",
-                               "--open", "-Pn", "-sS", "-oN", nmap_normal_output_file, "-oX", nmap_xml_output_file]
-        nmap = Nmapscan(nmap_executable, scan_file_plain,
-                        nmap_scan_arguments)
-        await nmap.start_scan()
-        await asyncio.gather(masscan.destruct(), nmap.destruct(), logrotate(
-            [nmap_normal_output_file, nmap_xml_output_file]))
+    #     date_time = datetime.today().strftime("%d-%m-%Y_%H-%M-%S")
+    #     nmap_normal_output_file = f"./open-proxy_{date_time}.txt"
+    #     nmap_xml_output_file = f"./open-proxy_{date_time}.xml"
+    #     nmap_scan_arguments = ["-vvv", "-n", "-T4", "--script", "http-open-proxy.nse", "-p", "3128",
+    #                            "--open", "-Pn", "-sS", "-oN", nmap_normal_output_file, "-oX", nmap_xml_output_file]
+    #     nmap = Nmapscan(nmap_executable, scan_file_plain,
+    #                     nmap_scan_arguments)
+    #     await nmap.start_scan()
+    #     nmap_destruct = asyncio.create_task(nmap.destruct())
+    #     logrotate_task = asyncio.create_task(logrotate(
+    #         [nmap_normal_output_file, nmap_xml_output_file]))
 
+    # await asyncio.gather(masscan_destruct, nmap_destruct, logrotate_task)
+    proxy = Proxyscan("./test.txt")
+    await proxy.test_proxies()
 
 if __name__ == '__main__':
     asyncio.run(main())
--- test.py
+++ test.py
@@ -5,15 +5,27 @@ import asyncio
 
 
 async def sleep_async():
-    await asyncio.sleep(10)
-    print("sleep_async done")
+    for i in range(15):
+        await asyncio.sleep(0.5)
+        print("sleep_async done")
     return 10
 
 
+async def sleep_async_2():
+    for i in range(15):
+        await asyncio.sleep(0.5)
+        print("sleep_async_2 done")
+    return 2
+
+
 async def main():
-    task = sleep_async()
-    print("main done")
-    oof = await task
+    while True:
+        tasks = []
+        tasks.append(asyncio.create_task(sleep_async()))
+        tasks.append(asyncio.create_task(sleep_async_2()))
+        await asyncio.sleep(5)
+        print("main done")
+    oof = await asyncio.gather(*tasks)
     print(oof)
 
 

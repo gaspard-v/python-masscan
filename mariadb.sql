6a2c82e7cda9025d8429238833d212ffd68e67bc
Gaspard VANDECASTEELE <gaspard.vandecasteele@student.yncrea.fr>
2022-10-16 21:15:26 +0200

    add database procedures

--- mariadb.sql
+++ mariadb.sql
@@ -3,7 +3,7 @@ USE openproxy;
 
 CREATE TABLE proxy (
     id BIGINT UNSIGNED AUTO_INCREMENT,
-    adresse VARCHAR(50) NOT NULL UNIQUE,
+    address VARCHAR(50) NOT NULL UNIQUE,
     ip_type TINYINT NOT NULL DEFAULT 4,
     add_date DATETIME NOT NULL DEFAULT NOW(),
     update_date DATETIME NOT NULL DEFAULT NOW(),
@@ -11,3 +11,24 @@ CREATE TABLE proxy (
     CONSTRAINT is_ip_type CHECK (ip_type = 4 OR ip_type = 6),
     PRIMARY KEY(id)
 );
+
+CREATE TABLE data (
+    id BIGINT UNSIGNED AUTO_INCREMENT,
+    data LONGBLOB,
+    filename TEXT NOT NULL,
+    add_date DATETIME NOT NULL DEFAULT NOW(),
+    PRIMARY KEY(id)
+);
+
+DELIMITER //
+CREATE PROCEDURE add_proxy (IN address VARCHAR(50), IN ip_type TINYINT)
+BEGIN
+    INSERT INTO proxy (address, ip_type) VALUES (address, ip_type)
+    ON DUPLICATE KEY UPDATE id=id, update_date=NOW(), update_count=update_count+1;
+END; //
+
+CREATE PROCEDURE add_data (IN data LONGBLOB, IN filename TEXT)
+BEGIN
+    INSERT INTO data (data, filename) VALUES (data, filename);
+END; //
+DELIMITER ;

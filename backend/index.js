4f17fbe16dea665ac613c440c5a0658e6f3e1d65
gaspard.vandecasteele <gaspard.vandecasteele@gmail.com>
2023-02-03 11:59:59 +0100

    add mariadb pool

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,17 @@
 "use strict";
 import Express from "express";
+import { createPool } from "mariadb";
 
 const app = Express();
 app.use(Express.json());
+
+const database = createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  connectionLimit: 100,
+});
 
 app.use((req, res, next) => {
   // use for development only !

0f511184255b68cd2aa4d6bea7eac1aa29c8cc19
gaspard.vandecasteele <gaspard.vandecasteele@gmail.com>
2023-02-03 08:36:28 +0100

    change mariadb file and add token

--- backend/mariadb.sql
+++ backend/mariadb.sql
@@ -25,6 +25,42 @@ CREATE TABLE data (
     PRIMARY KEY(id)
 );
 
+CREATE TABLE token_state_t (
+    id SERIAL,
+    token_state VARCHAR(20) NOT NULL UNIQUE DEFAULT "valid",
+    PRIMARY KEY(id)
+);
+
+INSERT INTO token_state_t (token_state) VALUES "valid, expired", "deleted", "invalid", "halted";
+
+CREATE TABLE permission_t (
+    id SERIAL,
+    permission VARCHAR(20) NOT NULL UNIQUE DEFAULT "read",
+    PRIMARY KEY(id)
+);
+
+INSERT INTO permission_t (permission) VALUES "proxy_read", "proxy_create", "proxy_update", "proxy_delete", "proxy_all", 
+                                             "token_read", "token_create", "token_update", "token_delete", "token_all",
+                                             "all_all";
+
+CREATE TABLE token (
+    id SERIAL,
+    token_state_fk SERIAL NOT NULL,
+    token CHAR(10) NOT NULL UNIQUE,
+    creation_date DATETIME NOT NULL DEFAULT NOW(),
+    modifiation_date DATETIME NOT NULL DEFAULT NOW(),
+    FOREIGN KEY (token_state) REFERENCE token_state_t(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE join_token_permission (
+    token_fk SERIAL,
+    permission_fk SERIAL,
+    FOREIGN KEY (token_fk) REFERENCE token(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (permission_fk) REFERENCE permission_t(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    PRIMARY KEY(token_fk, permission_fk)
+);
+
 DELIMITER //
 CREATE PROCEDURE add_proxy (IN in_address VARCHAR(50), IN in_port INT UNSIGNED, IN in_ip_type TINYINT, IN in_methodes VARCHAR(100), IN in_scan_date BIGINT UNSIGNED, IN in_commentaire TEXT)
 BEGIN

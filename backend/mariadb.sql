7c1a9b31140ad03a4f889c135b659ea5b27b2356
gaspard.vandecasteele <gaspard.vandecasteele@gmail.com>
2023-02-03 11:46:28 +0100

    change mariadb.sql
    it was not a valid sql code

--- backend/mariadb.sql
+++ backend/mariadb.sql
@@ -31,7 +31,7 @@ CREATE TABLE token_state_t (
     PRIMARY KEY(id)
 );
 
-INSERT INTO token_state_t (token_state) VALUES "valid, expired", "deleted", "invalid", "halted";
+INSERT INTO token_state_t (token_state) VALUES ("valid"), ("expired"), ("deleted"), ("invalid"), ("halted");
 
 CREATE TABLE permission_t (
     id SERIAL,
@@ -39,25 +39,25 @@ CREATE TABLE permission_t (
     PRIMARY KEY(id)
 );
 
-INSERT INTO permission_t (permission) VALUES "proxy_read", "proxy_create", "proxy_update", "proxy_delete", "proxy_all", 
-                                             "token_read", "token_create", "token_update", "token_delete", "token_all",
-                                             "all_all";
+INSERT INTO permission_t (permission) VALUES ("proxy_read"), ("proxy_create"), ("proxy_update"), ("proxy_delete"), ("proxy_all"), 
+                                             ("token_read"), ("token_create"), ("token_update"), ("token_delete"), ("token_all"),
+                                             ("all_all");
 
 CREATE TABLE token_t (
     id SERIAL,
-    token_state_fk SERIAL NOT NULL,
+    token_state_fk BIGINT UNSIGNED NOT NULL,
     token CHAR(10) NOT NULL UNIQUE,
     creation_date DATETIME NOT NULL DEFAULT NOW(),
     modifiation_date DATETIME NOT NULL DEFAULT NOW(),
-    FOREIGN KEY (token_state) REFERENCE token_state_t(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (token_state_fk) REFERENCES token_state_t(id) ON UPDATE CASCADE ON DELETE CASCADE,
     PRIMARY KEY(id)
 );
 
 CREATE TABLE join_token_permission (
-    token_fk SERIAL,
-    permission_fk SERIAL,
-    FOREIGN KEY (token_fk) REFERENCE token_t(id) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (permission_fk) REFERENCE permission_t(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    token_fk BIGINT UNSIGNED,
+    permission_fk BIGINT UNSIGNED,
+    FOREIGN KEY (token_fk) REFERENCES token_t(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (permission_fk) REFERENCES permission_t(id) ON UPDATE CASCADE ON DELETE CASCADE,
     PRIMARY KEY(token_fk, permission_fk)
 );
 
@@ -81,7 +81,7 @@ BEGIN
     DECLARE var_date DATETIME;
 
     SET var_date = FROM_UNIXTIME(in_date);
-    SET var_token_state_id = SELECT id FROM token_state_t WHERE token_state = in_token_state;
+    SELECT id INTO var_token_state_id FROM token_state_t WHERE token_state = in_token_state;
 
     INSERT INTO token_t (token, token_state_fk, creation_date, modifiation_date) VALUES (in_token, var_token_state_id, var_date, var_date)
     ON DUPLICATE KEY UPDATE id=id, token_state_fk=var_token_state_id, modifiation_date=var_date;
@@ -92,10 +92,20 @@ BEGIN
     DECLARE var_permission_id BIGINT UNSIGNED;
     DECLARE var_token_id BIGINT UNSIGNED;
 
-    SET var_permission_id = SELECT id FROM permission_t WHERE permission = in_permission;
-    SET var_token_id = SELECT id FROM token_t WHERE token = in_token;
+    SELECT id INTO var_permission_id FROM permission_t WHERE permission = in_permission;
+    SELECT id INTO var_token_id FROM token_t WHERE token = in_token;
 
     INSERT INTO join_token_permission(token_fk, permission_fk) VALUES (var_token_id, var_permission_id);
     UPDATE token_t SET modifiation_date = NOW() WHERE id = var_token_id;
+END; //
+
+CREATE PROCEDURE delete_token_permission (IN in_token CHAR(10), in_permission VARCHAR(10))
+BEGIN
+    DECLARE var_permission_id BIGINT UNSIGNED;
+    DECLARE var_token_id BIGINT UNSIGNED;
+
+    SELECT id INTO var_permission_id FROM permission_t WHERE permission = in_permission;
+    SELECT id INTO var_token_id FROM token_t WHERE token = in_token;
+    DELETE FROM join_token_permission WHERE token_fk=var_token_id AND permission_fk=var_permission_id;
 END; //
 DELIMITER ;

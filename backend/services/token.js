aa8dea30825f98228927caf19244ef655af45c0d
Gaspard VANDECASTEELE <gaspard.vandecasteele@student.yncrea.fr>
2023-02-08 16:00:36 +0100

    typo

--- backend/services/token.js
+++ backend/services/token.js
@@ -2,5 +2,5 @@ import database from "./mariadb.js";
 
 async function check_permission({ token, permission }) {
   const conn = await database.getConnection();
-  const result = await conn.query("CALL get_token_permission(?)", [permission]);
+  const result = await conn.query("CALL get_token_permission(?)", [token]);
 }

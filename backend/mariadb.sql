1b15b42c01e9393a3e51fb522f704023cdba7c1b
gaspard.vandecasteele <gaspard.vandecasteele@gmail.com>
2023-02-10 12:03:31 +0100

    change docker file and add openproxy user

--- backend/mariadb.sql
+++ backend/mariadb.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE openproxy CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS openproxy CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE openproxy;
 
 CREATE TABLE proxy (

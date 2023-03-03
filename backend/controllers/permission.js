d806a7970a1a0571f829e09b56e9c8a74b532d6b
gaspard.vandecasteele <gaspard.vandecasteele@gmail.com>
2023-03-03 16:15:19 +0100

    add corection

--- backend/controllers/permission.js
+++ backend/controllers/permission.js
@@ -3,20 +3,20 @@ import { check_permission as cp } from "../services/token";
 export async function check_permission(req, res, next) {
   try {
     const token_header = req.headers["authorization"];
-    if (!token_header) res.send(401, "No token provided");
+    if (!token_header) return res.send(401, "no token provided");
     const [token_type, token] = token_header.split(" ");
-    if (!token) res.send(401);
+    if (!token || !token_type)
+      return res.send(401, "invalide authorization header");
     const obj = req.baseUrl.substring(1);
     const method = req.path.substring(1);
+    if (!obj || !method) return res.send(400, "invalid request");
     const result_token = await cp({
       token: token,
       permission: `${obj}_${method}`,
     });
-    if (!result_token) {
-      res.send(401, "not permitted");
-    }
+    if (!result_token) return res.send(401, "not permitted");
     next();
   } catch {
-    res.send(400, "invalid token");
+    res.send(500, "serveur error");
   }
 }

4aabf6f0f0d8ad3183853232971ea063de846869
Gaspard <gaspard.vandecasteele@gmail.com>
2023-03-20 17:08:24 +0100

    add http-status

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 });
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send("Error");
+  res.status(500).json({});
 });
 
 app.use("/token", check_permission);

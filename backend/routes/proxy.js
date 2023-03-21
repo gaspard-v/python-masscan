6513d2ebd2ea7a25f4405cb872429eafdd4b2bb0
Gaspard <gaspard.vandecasteele@gmail.com>
2023-03-21 15:57:42 +0100

    fix

--- backend/routes/proxy.js
+++ backend/routes/proxy.js
@@ -10,6 +10,8 @@ router.route("/").get(async (req, res) => {
 router.route("/create").post(async (req, res) => {
   try {
     const response = await create_proxy({ body: req.body });
+    const [[{ update_count }]] = response;
+    if (update_count > 0) return res.status(200).json(response);
     res.status(201).json(response);
   } catch (err) {
     if (err instanceof TypeError) {
